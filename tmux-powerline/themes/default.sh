cc0c60cd58135a165f8c7e95d8faf5cc3f9e4152
Brooks Swinnerton <bswinnerton@gmail.com>
2015-03-23 11:15:29 -0400

    Removing tmux powerline fanciness

--- tmux-powerline/themes/default.sh
+++ tmux-powerline/themes/default.sh
@@ -41,13 +41,13 @@ if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
 		"pwd 89 211" \
-		"mailcount 9 255" \
-		"now_playing 234 37" \
+		#"mailcount 9 255" \
+		#"now_playing 234 37" \
 		#"cpu 240 136" \
 		"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
 		"battery 137 127" \
-		"weather 37 255" \
+		#"weather 37 255" \
 		#"rainbarf 0 0" \
 		#"xkb_layout 125 117" \
 		"date_day 235 136" \
