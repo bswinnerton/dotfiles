66e52b246148265f7eebbd3363c710bd1341a7ca
Brooks Swinnerton <bswinnerton@gmail.com>
2015-12-01 11:11:05 -0500

    Reorganizing PATH

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,13 +1,13 @@
-export PATH="./bin:$PATH"
-export PATH="$HOME/.rbenv/bin:$PATH"
-
 case "$(uname -s)" in
   Darwin)
-    export PATH="/usr/local/bin:$PATH"
     export PATH="/usr/local/heroku/bin:$PATH"
+    export PATH="/usr/local/bin:$PATH"
     ;;
 
   Linux)
     export PATH="$HOME/.linuxbrew/bin:$PATH"
     ;;
 esac
+
+export PATH="./bin:$PATH"
+export PATH="$HOME/.rbenv/bin:$PATH"
