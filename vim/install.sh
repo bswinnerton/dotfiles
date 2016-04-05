2bc7776b83f638fcd47723a2b198741f4493cd69
Brooks Swinnerton <bswinnerton@gmail.com>
2016-04-05 14:08:26 -0400

    Removing references to Neovim

--- vim/install.sh
+++
@@ -1,9 +0,0 @@
-# Sets up Neovim config directories
-
-if [ -d ~/.config/nvim ]; then
-  ln -s ~/.vim ~/.config/nvim
-fi
-
-if [ -d ~/.config/nvim/init.vim ]; then
-  ln -s ~/.vimrc ~/.config/nvim/init.vim
-fi
--- vim/vim.symlink/.vim
+++
@@ -1,1 +0,0 @@
-/Users/brooks/.vim
