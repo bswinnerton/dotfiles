20f563dfcdf5c54c5ca633bd33078eb5b7f8d801
Brooks Swinnerton <bswinnerton@gmail.com>
2016-04-10 21:28:36 -0400

    Updating FZF scripts to use brew prefix

--- fzf/env.zsh
+++ fzf/env.zsh
@@ -1,22 +1,22 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == *$(brew --prefix)/opt/fzf/bin* ]]; then
+  export PATH="$PATH:$(brew --prefix)/opt/fzf/bin"
 fi
 
 # Man path
 # --------
-if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
+if [[ ! "$MANPATH" == *$(brew --prefix)/opt/fzf/man* && -d "$(brew --prefix)/opt/fzf/man" ]]; then
+  export MANPATH="$MANPATH:$(brew --prefix)/opt/fzf/man"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$(brew --prefix)/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-# source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+# source "$(brew --prefix)/opt/fzf/shell/key-bindings.zsh"
 
 # Configuration Options
 # ---------------------
