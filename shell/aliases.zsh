85da088c4d89d0c14eacc452abfd3967a2ccab53
Brooks Swinnerton <bswinnerton@gmail.com>
2016-04-25 11:03:19 -0400

    Set the default Vim executable to MacVim's
    But only if MacVim is installed.

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -7,6 +7,10 @@ alias redraw="stty sane && clear"
 alias tmux="tmux -2"
 alias vi="vim"
 
+if command -v mvim >/dev/null 2>&1; then
+  alias vim="mvim -v"
+fi
+
 case "$(uname -s)" in
   Darwin)
     alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
