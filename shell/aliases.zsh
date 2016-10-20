9861bd8941bb777d86cf06bc5d46c036f67f9c76
Brooks Swinnerton <bswinnerton@gmail.com>
2016-10-20 10:59:01 -0400

    Add webhook-proxy command

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -7,6 +7,7 @@ alias ls="ls -GpF"
 alias redraw="stty sane && clear"
 alias tmux="tmux -2"
 alias vi="vim"
+alias webhook-proxy="ngrok http 4567 --hostname bswinnerton.ngrok.io"
 
 if $(command -v mvim >/dev/null 2>&1); then
   alias vim="mvim -v"
