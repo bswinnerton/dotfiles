c3c13639093f2e984b894614285c9a6885019cc5
Brooks Swinnerton <bswinnerton@gmail.com>
2016-12-28 11:52:48 -0500

    Use vanilla vim in terminal instead of macvim

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -9,10 +9,6 @@ alias tmux="tmux -2"
 alias vi="vim"
 alias webhook-proxy="ngrok http 4567 --hostname bswinnerton.ngrok.io"
 
-if $(command -v mvim >/dev/null 2>&1); then
-  alias vim="mvim -v"
-fi
-
 case "$(uname -s)" in
   Darwin)
     alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
