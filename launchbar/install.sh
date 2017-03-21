dc92810d08f6d5b0b6d020aaed0e693bfb342d8a
Brooks Swinnerton <bswinnerton@gmail.com>
2017-03-20 21:23:51 -0400

    Add LaunchBar snippets

--- launchbar/install.sh
+++ launchbar/install.sh
@@ -5,4 +5,5 @@ if ! [ -f ~/Library/Application\ Support/LaunchBar/Actions ]; then
   echo "Creating alias to Launchbar actions in dotfiles"
   mv ~/Library/Application\ Support/LaunchBar/Actions ~/Library/Application\ Support/LaunchBar/Old\ Actions
   ln -s ~/.dotfiles/launchbar ~/Library/Application\ Support/LaunchBar/Actions
+  ln -s ~/.dotfiles/launchbar/snippets ~/Library/Application\ Support/LaunchBar/Snippets
 fi
