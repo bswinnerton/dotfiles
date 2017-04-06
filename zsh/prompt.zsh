137ce23e4457df0b6cfe0336a4ce4c142a4c4175
Brooks Swinnerton <bswinnerton@gmail.com>
2017-04-05 23:18:56 -0400

    Change git part of prompt to use ✔ and ✘

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -29,12 +29,12 @@ git_branch() {
 
 git_dirty() {
   if $(! $git status -s --ignore-submodules=dirty &> /dev/null); then
-    echo "%{$fg[red]%}♥%{$reset_color%}"
+    echo "%{$fg[red]%} ✔ %{$reset_color%}"
   else
     if [[ $($git status --porcelain --ignore-submodules=dirty) == "" ]]; then
-      echo "%{$fg[red]%}♥%{$reset_color%}"
+      echo "%{$fg[red]%} ✔ %{$reset_color%}"
     else
-      echo "%{$fg[red]%}♨%{$reset_color%}"
+      echo "%{$fg[red]%} ✘ %{$reset_color%}"
     fi
   fi
 }
@@ -49,13 +49,13 @@ need_push_or_pull() {
     echo ""
   elif [[ $head == $base ]]; then
     # Need pull
-    echo "%{$fg_bold[magenta]%} ⬇%{$reset_color%}"
+    echo "%{$fg_bold[magenta]%}⬇%{$reset_color%}"
   elif [[ $remote_head == $base ]]; then
     # Need push
-    echo "%{$fg_bold[magenta]%} ⬆%{$reset_color%}"
+    echo "%{$fg_bold[magenta]%}⬆%{$reset_color%}"
   else
     # Diverged
-    echo "%{$fg_bold[magenta]%} ➡%{$reset_color%}"
+    echo "%{$fg_bold[magenta]%}➡%{$reset_color%}"
   fi
 }
 
