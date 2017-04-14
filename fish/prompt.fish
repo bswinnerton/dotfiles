e355cc393238f6a6197c45b41a9352963b7cdb48
Brooks Swinnerton <bswinnerton@gmail.com>
2017-04-14 00:29:07 -0400

    Add prompt

---
+++ fish/prompt.fish
@@ -0,0 +1,41 @@
+set __fish_git_prompt_show_informative_status 'yes'
+
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'no'
+set __fish_git_prompt_showuntrackedfiles 'no'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_dirtystate red
+set __fish_git_prompt_color_stagedstate red
+set __fish_git_prompt_color_untrackedfiles red
+set __fish_git_prompt_color_upstream_ahead red
+set __fish_git_prompt_color_upstream_behind red
+set __fish_git_prompt_color_upstream_behind red
+
+set __fish_git_prompt_char_cleanstate '✔'
+set __fish_git_prompt_char_dirtystate '✘'
+set __fish_git_prompt_char_stagedstate '●'
+set __fish_git_prompt_char_untrackedfiles '+'
+set __fish_git_prompt_char_upstream_ahead '⬇'
+set __fish_git_prompt_char_upstream_behind '⬆'
+set __fish_git_prompt_char_stateseparator ' '
+
+function fish_prompt
+  set_color green
+  printf '%s' (whoami)@(hostname)
+
+  set_color normal
+  printf ':'
+
+  set_color blue
+  printf '%s' (prompt_pwd)
+
+  set_color normal
+  printf '|'
+
+  set_color yellow
+  printf '%s' (__fish_git_prompt '%s')
+
+  set_color normal
+
+  printf '\n$ '
+end
