96c92687061a0b483ea46ef6a97506a586982e6c
Brooks Swinnerton <bswinnerton@gmail.com>
2017-04-14 00:36:42 -0400

    Flip upstream ahead / behind

--- fish/prompt.fish
+++ fish/prompt.fish
@@ -14,8 +14,8 @@ set __fish_git_prompt_char_cleanstate '✔'
 set __fish_git_prompt_char_dirtystate '✘'
 set __fish_git_prompt_char_stagedstate '●'
 set __fish_git_prompt_char_untrackedfiles '+'
-set __fish_git_prompt_char_upstream_ahead '⬇ '
-set __fish_git_prompt_char_upstream_behind '⬆ '
+set __fish_git_prompt_char_upstream_behind '⬇ '
+set __fish_git_prompt_char_upstream_ahead '⬆ '
 set __fish_git_prompt_char_stateseparator ' '
 
 function fish_prompt
