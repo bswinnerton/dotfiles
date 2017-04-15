195e4680e7d7196f3c565ccf0de3c9d4603aa1db
Brooks Swinnerton <bswinnerton@gmail.com>
2017-04-15 17:37:33 -0400

    Update aliases to be Fish compatible

--- shell/aliases.zsh
+++ shell/aliases.zsh
@@ -4,15 +4,9 @@ alias gist="gist -p"
 alias git="/usr/local/bin/git"
 alias gti="git"
 alias ls="ls -GpF"
-alias redraw="stty sane && clear"
+alias redraw="stty sane; clear"
+alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
 alias tmux="tmux -2"
 alias vi="vim"
 alias vim="nvim"
 alias webhook-proxy="ngrok http 4567 --hostname bswinnerton.ngrok.io"
-
-case "$(uname -s)" in
-  Darwin)
-    alias subl="/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl"
-    alias slime="subl"
-    ;;
-esac
