aaaef852bbe992ade1a81023ed74224aad003b54
Brooks Swinnerton <bswinnerton@gmail.com>
2018-10-04 09:21:20 -0400

    Try to create directory if not exist

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -7,7 +7,7 @@
 
 echo "Installing Homebrew for you."
 
-sudo mkdir /usr/local/sbin/
+sudo mkdir -p /usr/local/sbin/
 sudo chown -R $(whoami) /usr/local/sbin
 
 # Install the correct homebrew for each OS type
