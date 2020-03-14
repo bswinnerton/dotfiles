d59f88ee3bba7e4d427e1458cf84b450e7feceee
Brooks Swinnerton <bswinnerton@gmail.com>
2020-03-14 10:46:41 -0400

    Remove flag to set stash state in git prompt
    Apparently the presence of _anything_ will now result in a flag unicode
    character.

--- fish/prompt.auto.fish
+++ fish/prompt.auto.fish
@@ -3,7 +3,6 @@
 set -g __fish_git_prompt_show_informative_status 'yes'
 
 set -g __fish_git_prompt_showdirtystate 'yes'
-set -g __fish_git_prompt_showstashstate 'no'
 set -g __fish_git_prompt_showuntrackedfiles 'no'
 set -g __fish_git_prompt_showupstream 'yes'
 set -g __fish_git_prompt_color_dirtystate red
