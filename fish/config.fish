8ec759610bacdeb3d88cf937ccec0f15be0cae48
Brooks Swinnerton <bswinnerton@gmail.com>
2021-04-11 10:54:48 -0400

    Use ripgrep for file globbing
    HOLY SHIT THIS IS SO FAST.

--- fish/config.fish
+++ fish/config.fish
@@ -5,16 +5,16 @@ if not status --is-interactive
 end
 
 # Autoload any files that update the $PATH first
-for file in ~/.dotfiles/**/path.fish
+for file in (rg --files ~/.dotfiles/ | rg path.fish)
   source $file
 end
 
 # Autoload any files that end in .auto.fish
-for file in ~/.dotfiles/**/*.auto.fish
+for file in (rg --files ~/.dotfiles/ | rg ".*\.auto.fish\z")
   source $file
 end
 
 # Autoload any files that end in auto.sh
-for file in ~/.dotfiles/**/*.auto.sh
+for file in (rg --files ~/.dotfiles/ | rg ".*\.auto.sh\z")
   sh $file
 end
