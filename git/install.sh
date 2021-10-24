9a93cd35089c91926596c199cbcbe8105e755747
Brooks Swinnerton <brooks@stripe.com>
2021-10-24 12:32:29 -0400

    Add further distribution checks

--- git/install.sh
+++ git/install.sh
@@ -1,2 +1,5 @@
-echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
-killall gpg-agent
+OS="`uname -s`"
+if [ $OS == "Darwin" ]; then
+  echo "pinentry-program /usr/local/bin/pinentry-mac" >> ~/.gnupg/gpg-agent.conf
+  killall gpg-agent
+fi
--- go/install.sh
+++ go/install.sh
@@ -1,1 +1,9 @@
 mkdir -p $HOME/go/bin
+
+OS="`uname -s`"
+if [ $OS == "Darwin" ]; then
+  brew install golang
+else
+  sudo apt-get update
+  sudo apt-get -y install golang
+fi
