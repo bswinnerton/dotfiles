6fb594dda0fffd08296437ca966a11fc49f76931
Brooks Swinnerton <bswinnerton@gmail.com>
2024-03-24 12:35:52 -0400

    Add sbin to path for mtr

--- shell/path.fish
+++ shell/path.fish
@@ -1,5 +1,6 @@
 set -g fish_user_paths ./bin $fish_user_paths
 set -g fish_user_paths ~/.dotfiles/bin $fish_user_paths
+set -g fish_user_paths /opt/homebrew/sbin $fish_user_paths
 
 if test -d (brew --prefix)/opt/curl/bin
   set -g fish_user_paths (brew --prefix)/opt/curl/bin $fish_user_paths
