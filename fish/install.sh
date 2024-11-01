dfae825d10817a02b529650cb24f5287e1d61cda
Brooks Swinnerton <bswinnerton@gmail.com>
2024-11-01 16:43:59 -0400

    Remove sudos

--- fish/install.sh
+++ fish/install.sh
@@ -14,7 +14,7 @@ current_user=$(whoami)
 fish_path=$(which fish)
 
 #FIXME: Suppress the $fish_path from being outputted on the screen
-grep -qF "fish" /etc/shells || echo $fish_path | sudo tee -a /etc/shells
+#grep -qF "fish" /etc/shells || echo $fish_path | sudo tee -a /etc/shells
 
 chsh -s $fish_path $current_user
 
--- go/install.sh
+++ go/install.sh
@@ -1,9 +1,13 @@
-mkdir -p $HOME/go/bin
+#!/bin/sh
 
-OS="`uname -s`"
-if [ $OS = "Darwin" ]; then
-  brew install golang
-else
-  sudo apt-get update
-  sudo apt-get -y install golang
+if ! which go > /dev/null 2>&1; then
+  mkdir -p "$HOME/go/bin"
+
+  OS="`uname -s`"
+  if [ "$OS" = "Darwin" ]; then
+    brew install golang
+  else
+    sudo apt-get update
+    sudo apt-get -y install golang
+  fi
 fi
