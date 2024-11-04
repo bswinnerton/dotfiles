ec366fcadbd9e74b937412c9332b2b1fde577190
Brooks Swinnerton <bswinnerton@gmail.com>
2024-11-04 12:18:56 -0500

    Migrate to fnm

--- node/node.auto.fish
+++ node/node.auto.fish
@@ -1,4 +1,7 @@
-if test -d $HOME/.nodenv
-  set -g fish_user_paths $HOME/.nodenv/bin $fish_user_paths
-  status --is-interactive; and source (nodenv init -|psub)
+if test -d $HOME/.local/share/fnm
+    set FNM_PATH "$HOME/.local/share/fnm"
+    if [ -d "$FNM_PATH" ]
+        set PATH "$FNM_PATH" $PATH
+        fnm env | source
+    end
 end
