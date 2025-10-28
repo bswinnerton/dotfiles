0b929f1429e6fdfe7b42d591c78f03545107b8fd
Brooks Swinnerton <bswinnerton@gmail.com>
2025-10-28 13:17:23 -0400

    Prepend ~/.dotfiles/bin/ before homebrew bin

--- shell/path.fish
+++ shell/path.fish
@@ -1,5 +1,3 @@
-set -gxp PATH ~/.dotfiles/bin
-
 if type -q brew
   set -g fish_user_paths /opt/homebrew/sbin $fish_user_paths
 
@@ -7,3 +5,6 @@ if type -q brew
     set -g fish_user_paths (brew --prefix)/opt/curl/bin $fish_user_paths
   end
 end
+
+# Ensure ~/.dotfiles/bin is always first in PATH
+set -g fish_user_paths ~/.dotfiles/bin $fish_user_paths
